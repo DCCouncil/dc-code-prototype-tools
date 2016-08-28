1c6d2c746b2b56fedd644b9554501d638b4831c7
David Greisen <dgreisen@dccouncil.us>
2016-08-28 09:37:57 -0400

    removed pygments

--- bill_watch.py
+++ bill_watch.py
@@ -2,17 +2,11 @@ import sys, time, os, lxml.etree as etree, re
 from parse_law import parse
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler, FileCreatedEvent, DirCreatedEvent
-from pygments.lexers import XmlLexer
-from pygments.formatters import HtmlFormatter
-from pygments import highlight
 from shutil import rmtree
 
 def is_docx(path):
     base = os.path.basename(path)
     return path.endswith('.docx') and not base.startswith('~')
-
-xml_lexer = XmlLexer()
-html_formatter = HtmlFormatter(style='igor')
 
 error_re = re.compile(r'errors="(\d+)"')
 
@@ -20,28 +14,6 @@ class EventHandler(FileSystemEventHandler):
     def __init__(self, in_path, out_path):
         self.in_path = in_path
         self.out_path = out_path
-
-    def make_index(self, src_path):
-        pyg_path = self.pygments_path(src_path)
-        children = os.listdir(pyg_path)
-        html = '<ul>'
-        for child in children:
-            if child == 'index.html':
-                continue
-            url_path = '#' + os.path.join(self.rel_path(src_path), child)
-            child_path = os.path.join(pyg_path, child)
-            glyph = '&#128194;'
-            error = ''
-            if not os.path.isdir(child_path):
-                glyph = '&nbsp;'
-                with open(os.path.join(self.raw_path(src_path), child)[:-4] + 'xml', 'r', encoding='utf-8') as f:
-                    match = error_re.search(f.readline())
-                    if match:
-                        error = ' <span class="error">({} errors)</span>'.format(match.group(1))
-            html += '<li>{} <a href="{}">{}</a>{}</li>'.format(glyph, url_path, child, error)
-        html += '</ul>'
-        with open(os.path.join(pyg_path, 'index.html'), 'w') as f:
-            f.write(html)
 
     def rel_path(self, src_path):
         return os.path.relpath(src_path, self.in_path)
@@ -52,22 +24,13 @@ class EventHandler(FileSystemEventHandler):
             path = path[:-4] + 'xml'
         return path
 
-    def pygments_path(self, src_path):
-        path = os.path.join(self.out_path, 'pygments', self.rel_path(src_path))
-        if is_docx(src_path):
-            path = path[:-4] + 'html'
-        return path
-
-    def make_dir(self, src_path, update_index=True):
+    def make_dir(self, src_path):
         try:
             os.makedirs(self.raw_path(src_path))
-            os.makedirs(self.pygments_path(src_path))
         except OSError:
             pass
-        if update_index:
-            self.make_index(src_path)
 
-    def make_file(self, src_path, update_index=True):
+    def make_file(self, src_path):
         try:
             dom = parse(src_path)
             xml = etree.tostring(dom, pretty_print=True, encoding="utf-8")
@@ -76,20 +39,13 @@ class EventHandler(FileSystemEventHandler):
             xml = '<error errors="1">A fatal parsing error occurred. Please contact support. {}</error>'.format(e).encode('utf-8')
         with open(self.raw_path(src_path), 'wb') as f:
             f.write(xml)
-        # html = highlight(xml, xml_lexer, html_formatter)
-        # with open(self.pygments_path(src_path), 'w') as f:
-        #     f.write(html)
-        # if update_index:
-        #     self.make_index(os.path.dirname(src_path))
 
     def delete_dir(self, src_path):
         rmtree(self.raw_path(src_path))
-        rmtree(self.pygments_path(src_path))
         self.make_index(os.path.dirname(src_path))
 
     def delete_file(self, src_path):
         os.remove(self.raw_path(src_path))
-        os.remove(self.pygments_path(src_path))
         self.make_index(os.path.dirname(src_path))
 
     def on_created(self, event):
@@ -122,31 +78,21 @@ class EventHandler(FileSystemEventHandler):
 def walk(event_handler):
     dirs_to_update = []
     for root, dirs, files in os.walk(event_handler.in_path):
-        event_handler.make_dir(root, False)
-        dirty = False
+        event_handler.make_dir(root)
         for f in files:
             src_path = os.path.join(root, f)
             if not is_docx(src_path):
                 continue
             raw_path = event_handler.raw_path(src_path)
-            pyg_path = event_handler.pygments_path(src_path)
             src_mod_time = os.path.getmtime(src_path)
             try:
                 raw_mod_time = os.path.getmtime(raw_path)
-                pyg_mod_time = os.path.getmtime(pyg_path)
             except FileNotFoundError:
-                dirty = True
-                event_handler.make_file(src_path, False)
+                event_handler.make_file(src_path)
             else:
-                if src_mod_time >= raw_mod_time or src_mod_time >= pyg_mod_time:
+                if src_mod_time >= raw_mod_time :
                     dirty = True
-                    event_handler.make_file(src_path, False)
-        if dirty:
-            dirs_to_update.append(root)
-
-
-    for path in dirs_to_update:
-        event_handler.make_index(path)
+                    event_handler.make_file(src_path)
 
 if __name__ == '__main__':
     observer = Observer()
